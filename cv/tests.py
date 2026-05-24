c4f26dca00b87d0d9c1e7c29d20db22702dbb946
Agustín José Fara (Stage) <agustin.fara@gmail.com>
2026-05-24 20:37:03 +0000

    Agrego edad automatica en cv:about

--- cv/tests.py
+++ cv/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.conf import settings
+from django.urls import reverse
 from .models import Work
 import datetime
+from unittest.mock import patch
+
 # Create your tests here.
 
 class WorkModelTests(TestCase):
@@ -47,26 +50,53 @@ class WorkModelTests(TestCase):
         work_diff = Work(datestart=test_date_st,datefinish=test_date_fi,title='test_diff')
         self.assertEqual(work_diff.time_worked(), '20 años y 2 meses')
 
-    def test_dates_time_worked(self):
+    def test_dates_time_worked_less_than_month(self):
         test_date_st = datetime.datetime(1990,4,15)
         test_date_fi = datetime.datetime(1990,5,10)
         work_diff = Work(datestart=test_date_st,datefinish=test_date_fi,title='test_diff')
         self.assertEqual(work_diff.time_worked(), 'Menos de un mes')
 
-    def test_dates_time_worked(self):
+    def test_dates_time_worked_one_month(self):
         test_date_st = datetime.datetime(1989,12,15)
         test_date_fi = datetime.datetime(1990,1,14)
         work_diff = Work(datestart=test_date_st,datefinish=test_date_fi,title='test_diff')
         self.assertEqual(work_diff.time_worked(), '1 mes')
 
-    def test_dates_time_worked(self):
+    def test_dates_time_worked_less_than_month_exact(self):
         test_date_st = datetime.datetime(1989,12,15)
         test_date_fi = datetime.datetime(1990,1,13)
         work_diff = Work(datestart=test_date_st,datefinish=test_date_fi,title='test_diff')
         self.assertEqual(work_diff.time_worked(), 'Menos de un mes')
 
-    def test_dates_time_worked(self):
+    def test_dates_time_worked_year_and_month(self):
         test_date_st = datetime.datetime(1980,5,1)
         test_date_fi = datetime.datetime(1981,6,1)
         work_diff = Work(datestart=test_date_st,datefinish=test_date_fi,title='test_diff')
         self.assertEqual(work_diff.time_worked(), '1 año y 1 mes')
+
+
+class TestEdadAbout(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('cv:about')
+
+    @patch('cv.views.date')
+    def test_edad_calculada_correctamente_en_cumpleaños(self, mock_datetime):
+        """Prueba que si hoy esel cumpleaños de 2024, la edad sea exactamente 40"""
+        mock_datetime.today.return_value = datetime.date(2024, 3, 4)
+        mock_datetime.side_effect = lambda *args, **kwargs: datetime.date(*args, **kwargs)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['edad'], 40)
+
+    @patch('cv.views.date')
+    def test_edad_un_dia_antes_del_cumpleaños(self, mock_datetime):
+        """Prueba que si hoy es un día antes de tu cumple en 2024, todavía devuelva 39"""
+        mock_datetime.today.return_value = datetime.date(2024, 3, 3)
+        mock_datetime.side_effect = lambda *args, **kwargs: datetime.date(*args, **kwargs)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['edad'], 39)
--- cv/urls.py
+++ cv/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 
-#patts de la app polls
+#paths de la app CV
 app_name = 'cv'
 urlpatterns = [
-    path('about', views.about, name='about'), #Página sobre mi  
+    path('about', views.about, name='about'), #Página sobre mi
     path('cv', views.cv, name='cv'), #Experiencia Laboral
     path('tech', views.tech, name='tech'), #Tecnologías
 ]
